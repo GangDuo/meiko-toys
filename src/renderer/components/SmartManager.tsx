f1b96b503772526e9abf97e8304aa999403d1efd
GangDuo <5295036+GangDuo@users.noreply.github.com>
2023-11-22 17:35:56 +0900

    ファイルをドラッグアンドドロップできる領域を追加した。

--- src/renderer/components/SmartManager.tsx
+++ src/renderer/components/SmartManager.tsx
@@ -1,7 +1,34 @@
+import { useDropzone } from 'react-dropzone';
+
 export default function SmartManager() {
+  const { acceptedFiles, getRootProps, getInputProps, isDragActive } =
+    useDropzone({
+      accept: {
+        'text/csv': ['.csv'],
+      },
+    });
+
+  const files = acceptedFiles.map((file) => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
   return (
     <section>
       <h1>KDDI Smart Mobile Safety Manager</h1>
+      <div {...getRootProps({ className: 'dropzone' })}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
+      <aside>
+        <h4>Files</h4>
+        <ul>{files}</ul>
+      </aside>
     </section>
   );
 }

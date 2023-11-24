78a80f73cbbc7f400656397c2cf756540bba246c
GangDuo <5295036+GangDuo@users.noreply.github.com>
2023-11-24 11:48:09 +0900

    用途を選択するドロップダウンを追加した。

--- src/renderer/components/SmartManager.tsx
+++ src/renderer/components/SmartManager.tsx
@@ -1,4 +1,9 @@
 import { useDropzone } from 'react-dropzone';
+import { useState } from 'react';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
+const options = ['教室長', '直営', '社員'];
 
 export default function SmartManager() {
   const { acceptedFiles, getRootProps, getInputProps, isDragActive } =
@@ -14,6 +19,15 @@ export default function SmartManager() {
     </li>
   ));
 
+  const [option, setOption] = useState({ selected: '' });
+
+  const onSelect = (op: Option) => {
+    console.log('You selected ', op.label);
+    setOption({ selected: op });
+  };
+
+  const defaultOption = option.selected;
+
   return (
     <section>
       <h1>KDDI Smart Mobile Safety Manager</h1>
@@ -25,6 +39,13 @@ export default function SmartManager() {
           <p>Drag 'n' drop some files here, or click to select files</p>
         )}
       </div>
+      <h3>用途</h3>
+      <Dropdown
+        options={options}
+        onChange={onSelect}
+        value={defaultOption}
+        placeholder="Select an option"
+      />
       <aside>
         <h4>Files</h4>
         <ul>{files}</ul>
